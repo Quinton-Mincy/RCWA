28da072dc78029eb19d9ee5edb7f4bbcd2edb36f
Quinton Mincy <73136662+Quinton-Mincy@users.noreply.github.com>
2025-01-27 07:40:15 -0500

    Update bandstructure.py

--- bandstructure.py
+++ bandstructure.py
@@ -1,5 +1,5 @@
 #Author(s): Quinton Mincy
-#Last Modified: 01/10/24
+#Last Modified: 01/26/24
 #Code Built Arround Alex Song's Examples for his Inkstone library: https://github.com/alexysong/inkstone
 
 
@@ -14,23 +14,33 @@ import math
 from inkstone import Inkstone
 from tqdm import tqdm
 import csv
-
+from typing import List, Tuple
 
 
 #program parameters
 c    = 299792458
 #should be multiples of 90
-num_points = 450
-# latc = 0.3
-# a1 = (-(1/2)*latc,latc*math.sqrt(3)/2)
-# a2 = ((1/2)*latc,latc*math.sqrt(3)/2 )
-latc = 1
-a1=(1,0)
-a2=(0,1)
+num_points = 180
+latc = 0.5
+a1 = (-(1/2)*latc,latc*math.sqrt(3)/2)
+a2 = ((1/2)*latc,latc*math.sqrt(3)/2)
+# a1 = (latc,0)
+# a2 = (-(1/2)*latc,latc*math.sqrt(3)/2)
+
+# a1 = (latc,0)
+# a2 = ((1/2)*latc,latc*math.sqrt(3)/2)
+
+# Define hexagon parameters
+center = (0, 0)  # Center of the hexagon
+radius = 0.216/2    # circumradius (microns)
+
+# latc = 1
+# a1=(1,0)
+# a2=(0,1)
 
 
 #find reciprocal lattice vectors
-def recipro(a1, a2):
+def recipro(a1, a2,k_point = False):
     """
     given two lattice vectors, give two reciprocal lattice vectors
     If one of the lattice vectors is zero, then the returned corresponding reciprocal lattice vector is float('inf').
@@ -64,11 +74,25 @@ def recipro(a1, a2):
             ar = np.abs(np.cross(a1, a2))  # area
             coef = 2 * np.pi / ar
 
-            b1 = (coef * a2[1], -coef * a2[0])
-            b2 = (-coef * a1[1], coef * a1[0])
+            b1 = np.array([coef * a2[1], -coef * a2[0]])
+            b2 = np.array([-coef * a1[1], coef * a1[0]])
 
-    return np.array([b1, b2])
+    # print(b1,b2)
+    return b1,b2
 
+#angle between reciprocal lattice vectors (radians)
+def find_phi(b1,b2):
+    dot = np.dot(b1,b2)
+    mag_b1 = np.linalg.norm(b1)
+    mag_b2 = np.linalg.norm(b2)
+
+    phi = np.arccos(dot/(mag_b1 * mag_b2))
+
+    return phi
+def k_corner(b1,b2):
+    corner = b1 + b2
+    # corner = corner / np.linalg.norm(corner)
+    return corner
 
 def freq2lambda(a,freq):
     #wavelegnth = lattice_constant/norm_freq
@@ -94,51 +118,66 @@ def wavevector(theta,phi,ω):
   
     return k
 
-# def recpV(a1,a2,k):
-#     Q = np.array([[0,-1],[1,0]])
-#     b1 = (2*math.pi) * ( np.matmul(Q,a2) / np.dot(a1,np.matmul(Q,a2)) )
-#     b2 = (2*math.pi) * ( np.matmul(Q,a1) / np.dot(a2,np.matmul(Q,a1)) )
-
-#     k1 = np.dot(k,b1) / np.dot(b1,b1)
-#     k2 = np.dot(k,b2) / np.dot(b2,b2)
-
-#     # k_recp = np.array([k1*b1, k2*b2 ])
-#     k_recp = np.array([b1,b2])
-    
-#     return k_recp
+# Function to generate hexagon vertices
+def generate_hexagon(center: Tuple[float, float], radius: float) -> List[Tuple[float, float]]:
+    cx, cy = center
+    vertices = [
+        (cx + radius * np.cos(2 * np.pi * i / 6), cy + radius * np.sin(2 * np.pi * i / 6))
+        for i in range(6)
+    ]
+    return vertices
 
 
 def plot_spectrum(epsilon,frequency):
     s = Inkstone()
     s.lattice = (a1,a2)
+
+    b1,b2 =  recipro(a1,a2)
+    corner = k_corner(b1,b2)
+    print(corner)
+    phi_bis = find_phi(b1,b2) / 2
+    phi_values_rad = np.linspace(phi_bis,0,num_points)
+    phi_values_rad = phi_values_rad[1:]
+
+    phi_values_deg = phi_values_rad *(180/math.pi)
+
+
+
+  
     s.num_g = 50
 
+    hexagon_vertices = generate_hexagon(center, radius)
+
     # s.AddMaterial(name='Au', epsilon=epsilon[0])
-    s.AddMaterial(name='Au', epsilon=12)
+    s.AddMaterial(name='Au', epsilon=9.61)
     s.AddLayer(name='in', thickness=0, material_background='vacuum')
     s.AddLayer(name='slab', thickness=0.5*latc, material_background='Au')
     s.AddLayerCopy(name='out', original_layer='in', thickness=0)
-    s.AddPatternDisk(layer='slab', pattern_name='disk', material='vacuum', radius=0.2*latc)
+    # s.AddPatternDisk(layer='slab', pattern_name='disk', material='vacuum', radius=0.216)
+    s.AddPatternPolygon(layer = 'slab',material = 'Au',vertices = hexagon_vertices,pattern_name = 'Hexagon')
 
     #set figure size
     plt.figure(figsize=(10, 6))
 
     #sweep through incident angle
-    theta_values = np.linspace(0,90,num_points)
-    theta_values_rad = [math.radians(the) for the in theta_values]
+    theta_values = np.linspace(-90,90,num_points,endpoint = False)
+    #exlude start value
+    theta_values = theta_values[1:]
+
+    theta_values_rad = [math.radians(theta) for theta in theta_values]
     transmission_values = []
     kx_values = []
 
-    for theta in tqdm(theta_values):
+    for theta,phi in tqdm(zip(theta_values,phi_values_deg)):
 
-        s.SetExcitation(theta=theta, phi=0, s_amplitude=1/np.sqrt(2), p_amplitude=1/np.sqrt(2))
+        s.SetExcitation(theta=theta, phi=phi, s_amplitude=1/np.sqrt(2), p_amplitude=1/np.sqrt(2))
         flux_in = []
         flux_out = []
 
         for i, nu in enumerate(frequency):
             # Update material properties and frequency
             # s.SetMaterial(name='Au', epsi=epsilon[i])
-            s.SetMaterial(name='Au', epsi=12)
+            s.SetMaterial(name='Au', epsi=9.61)
             s.SetFrequency(nu)
             
             flux_in.append(s.GetPowerFlux('in'))
@@ -146,6 +185,7 @@ def plot_spectrum(epsilon,frequency):
 
 
         incident = np.array([a[0] for a in flux_in])
+     
         reflection = -np.array([a[1] for a in flux_in]) / incident
         transmission = np.array([a[0] for a in flux_out]) / incident
 
@@ -157,12 +197,17 @@ def plot_spectrum(epsilon,frequency):
         plt.plot(wavelengths, transmission * 100, label=f'Theta = {theta}°')
 
     #at each frequency, calculate projection of incident wavevectors onto reciprocal lattice
+    
+    k_point = (1/3)*b1 + (1/3)*b2
+    k_hat = k_point / np.linalg.norm(k_point)  # Normalize
+    k_hat_b1 = b1 / np.linalg.norm(b1)  # Normalize
+    
+    # print(k_hat_b1)
     for nu in frequency:
-        wavevectors = [wavevector(ang,0,nu) for ang in theta_values_rad]
+        wavevectors = [wavevector(theta,phi,nu) for theta,phi in zip(theta_values_rad,phi_values_rad)]
 
-        k_recps =  recipro(a1,a2)
 
-        projections = [proj(wv,k_recps[0]) for wv in wavevectors] 
+        projections = [proj(wv,corner) for wv in wavevectors] 
         proj_mags = [np.linalg.norm(proj) for proj in  projections]
         kx_values.append(proj_mags)
     
@@ -173,7 +218,6 @@ def plot_spectrum(epsilon,frequency):
     plt.title('Transmission vs Wavelength for Various Theta Values')
     plt.legend(loc='best')  # Automatically place legend
     plt.grid(True)
-    plt.tight_layout()  # Adjust layout to prevent clipping
 
     # Prepare data for contour plot
     kx_values = np.array(kx_values)
@@ -181,10 +225,10 @@ def plot_spectrum(epsilon,frequency):
     transmission_values_2d = transmission_values.T 
     frequency_2d = np.tile(frequency[:, np.newaxis], (1, kx_values.shape[1])) 
 
+
     #image show transmission
     plt.figure(figsize=(10, 6))
     plt.imshow(transmission_values_2d,cmap = 'viridis')
-    plt.tight_layout()
 
 
     #Contour map
@@ -194,7 +238,6 @@ def plot_spectrum(epsilon,frequency):
     plt.xlabel('$k_x$')
     plt.ylabel('Frequency ')
     plt.title('Transmission Intensity Contour ($k_x$ vs. Wavelength)')
-    plt.tight_layout()
 
 
 def extract(data):
