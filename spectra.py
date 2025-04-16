af1b7ef3e963e1f57f2249ec361e3ed8bd6afe84
Quinton Mincy <73136662+Quinton-Mincy@users.noreply.github.com>
2025-04-16 13:03:40 -0400

    Update spectra.py

--- spectra.py
+++ spectra.py
@@ -1,5 +1,5 @@
 #Author(s): Quinton Mincy
-#Last Modified: 01/10/24
+#Last Modified: 04/16/25
 #Code Built Arround Alex Song's Examples for his Inkstone library: https://github.com/alexysong/inkstone
 
 
@@ -27,8 +27,8 @@ pi   = np.pi
 #multipliers
 tera = 1e12
 #should be multiples of 3
-NUM_POINTS = 360
-NUM_G = 100
+NUM_POINTS = 180
+NUM_G = 50
 #microns
 thickness = 1
 
@@ -41,55 +41,80 @@ a1 = (latc,0)
 a2 = ((1/2)*latc,latc*math.sqrt(3)/2)
 
 
-def init_inkstone(epsilon,geometry,thickness):
+def init_inkstone(epsilon,core,shell,thickness):
     s = Inkstone()
     s.lattice = ((a1,a2))
     s.num_g = NUM_G
 
-    s.AddMaterial(name='InP', epsilon=epsilon.real)
+    s.AddMaterial(name='InP', epsilon=epsilon)
+    s.AddMaterial(name='Al2O3', epsilon=2.9467)
+
+    # s.AddLayer(name='in', thickness=0, material_background='vacuum')
+
+    # s.AddLayer(name='wire', thickness=thickness, material_background='vacuum') 
+    # # Add the outer Al2O3 shell
+    # s.AddPatternPolygon(layer='wire', material='Al2O3', pattern_name='shell', vertices=shell)
+    # # Add the inner InP core (must be completely inside the shell)
+    # s.AddPatternPolygon(layer='wire', material='InP', pattern_name='core', vertices=core)
+    # s.AddLayer(name='out', thickness=0, material_background='InP')
+
+    # s.AddLayer(name='in', thickness=0, material_background='vacuum')
+    # s.AddLayer(name='shell', thickness=0.01, material_background='Al2O3')  # outer shell is background
+    # s.AddLayer(name='wire', thickness=thickness, material_background='InP')  # outer shell is background
+    # s.AddLayer(name='out', thickness=0, material_background='InP')  # substrate
+    # s.AddPatternPolygon(layer="wire", material="InP", pattern_name="wire", vertices=core)
+
     
-    s.AddLayer(name='in', thickness=0, material_background='InP')
-    s.AddLayer(name='slab', thickness=thickness, material_background='InP')
-    s.AddLayer(name='out', thickness=0, material_background='vacuum')
+    s.AddLayer(name='in', thickness=0, material_background='vacuum')
+    s.AddLayer(name='shell', thickness=0.01, material_background='vacuum')
+    s.AddLayer(name='wire', thickness=thickness, material_background='Al2O3')
+    s.AddLayer(name='out', thickness=0, material_background='InP')
+    s.AddPatternPolygon(layer="shell", material="Al2O3", pattern_name="poly1",
+        vertices=shell)
+    s.AddPatternPolygon(layer="wire", material="InP", pattern_name="poly2",
+    vertices=core)
 
-    s.AddPatternPolygon(layer="slab", material="InP", pattern_name="poly1",
-        vertices=geometry)
+    # s.AddLayer(name='in', thickness=0, material_background='InP')
+    # s.AddLayer(name='wire', thickness=thickness, material_background='InP')
+    # s.AddLayer(name='out', thickness=0, material_background='vacuum')
+    # s.AddPatternPolygon(layer="wire", material="InP", pattern_name="poly1",
+    #     vertices=core)
 
+    # s.AddLayer(name='in', thickness=0, material_background='vacuum')
+    # s.AddLayer(name='wire', thickness=thickness, material_background='InP')
+    # s.AddLayer(name='out', thickness=0, material_background='vacuum')
+    # s.AddPatternPolygon(layer="wire", material="InP", pattern_name="poly1",
+    #     vertices=core)
     return s
 
-def plot_spectrum(ax, wavelength, transmission, thickness):
+def plot_spectrum(ax, wavelength, transmission,reflection,absorbance, thickness):
     # Plot transmission vs. wavelength on the provided axes.
     # Using a label to differentiate different thicknesses.
-    ax.plot(wavelength, transmission * 100, label=f'{thickness*1000:.0f} nm')
+    ax.plot(wavelength, transmission * 100, label='transmission')
+    ax.plot(wavelength, reflection * 100, label='reflection')
+    ax.plot(wavelength, absorbance * 100, label='absorbance')
+
 
 def RCWA(epsilon,frequency,wavelength):
     #normalize frequency for RCWA calculations
     frequency_norm = normalize_frequency(frequency,(latc_nm))
-    #create geometry
-    # centers=[(0,0),(latc,0),(latc/2,latc*np.sqrt(3)/2 )]
-    # geometry = init_geometry(latc/2,centers)
-    center = [0,0]
-    radius = 0.216/2
-    # center = [latc/2,latc*np.sqrt(3)/2]
-    geometry = generate_hexagon(center,radius )
-
-    # thickness = [0.015,0.018,0.02,0.025]#microns
+    #init geometry
+    shell = generate_hexagon(center=(0, 0), radius=118e-3)  # microns
+    core = generate_hexagon(center=(0, 0), radius=108e-3)
     thickness = [1]
 
     fig, ax = plt.subplots(figsize=(10, 6))
 
     for thick in tqdm(thickness):
-        #initialize inksone object
-        s = init_inkstone(epsilon[0],geometry,thick)
-        s.SetExcitation(theta = 0, phi=0, s_amplitude=1/np.sqrt(2), p_amplitude=1/np.sqrt(2))
         #arrays for incidence, transmission, refelction
         flux_in = []
         flux_out = []
 
         for i,nu in enumerate(frequency_norm):
-            # Update material properties and frequency
-            # print(f'freq:{frequency[i]/tera}, wav: {wavelength[i]}, norm_freq: {frequency_norm[i]}, eps: {epsilon[i]}')
-            s.SetMaterial(name='InP', epsi=epsilon[i].real)
+            s = init_inkstone(epsilon[i], core, shell, thick) 
+            # s = init_inkstone(11.56, core, shell, thick) 
+
+            s.SetExcitation(theta=6, phi=0, s_amplitude=1, p_amplitude=0)
             s.SetFrequency(nu)
             
             flux_in.append(s.GetPowerFlux('in'))
@@ -97,17 +122,21 @@ def RCWA(epsilon,frequency,wavelength):
 
 
         incident = np.array([a[0] for a in flux_in])
-        # reflection = -np.array([a[1] for a in flux_in]) / incident
+        reflection = -np.array([a[1] for a in flux_in]) / incident
         transmission = np.array([a[0] for a in flux_out]) / incident
-        # absorbance = transmission**-1
+        absorbance = 1 - transmission - reflection
 
-        plot_spectrum(ax,wavelength,transmission,thick)
+
+        plot_spectrum(ax,wavelength,transmission,reflection,absorbance,thick)
     
     # After the loop, label and display the figure
     ax.set_xlabel('Wavelength (nm)')
     ax.set_ylabel('Transmission (%)')
     ax.set_title('Transmission vs Wavelength for Various Thicknesses')
     ax.grid(True)
+    # ax.set_xlim(500, 1000)
+
+    # ax.set_ylim(0, 100)
     ax.legend(title='Thickness')
     plt.tight_layout()
     plt.show()
@@ -129,26 +158,10 @@ def freq2lambda(freq):
 def normalize_frequency(freq,a):
     return freq*(a/c)
 
-def parametric_circle(theta, center, radius):
-    x = center[0] + radius * np.cos(theta)
-    y = center[1] + radius * np.sin(theta)
-    return np.array(list(zip(x, y)))  # Convert to array of (x, y) tuples
-
-def init_geometry(radius,centers):
-    theta1 = np.linspace(np.pi/3,0,NUM_POINTS,endpoint=False)
-    theta2 = np.linspace(np.pi,2*np.pi/3, NUM_POINTS,endpoint=False)
-    theta3 = np.linspace(5*np.pi/3,4*np.pi/3,NUM_POINTS,endpoint=False)
-    thetas = [theta1,theta2,theta3]
-    sides = []
-    
-    for theta,center in zip(thetas,centers):
-        sides.append(parametric_circle(theta,center,radius))
-
-    geometry = np.concatenate((sides[0],sides[1],sides[2]))
-    return geometry
 
 # Function to generate hexagon vertices
 def generate_hexagon(center,radius):
+
     cx, cy = center
     vertices = [
         (cx + radius * np.cos(2 * np.pi * i / 6), cy + radius * np.sin(2 * np.pi * i / 6)) for i in range(6)]
